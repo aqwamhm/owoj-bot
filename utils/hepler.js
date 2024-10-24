057bfcd5bb1aeec36097c1a89446a612bf5f5a80
Aqwam Hizbal Muhshiy <aqwam.hizbal@outlook.com>
2024-10-24 18:46:44 +0700

    update get periodDate to accept period start and end from env

--- utils/hepler.js
+++ utils/hepler.js
@@ -2,21 +2,25 @@ const getPeriodDate = (period = 0) => {
     const now = new Date();
     const currentDay = now.getDay();
 
+    const startDay = parseInt(process.env.PERIOD_START_DAY || "1", 10);
+    const startHour = parseInt(process.env.PERIOD_START_HOUR || "0", 10);
     const daysToAdjust = period * 7;
 
-    const monday = new Date(now);
-    monday.setDate(
-        now.getDate() - (currentDay === 0 ? 6 : currentDay - 1) + daysToAdjust
-    );
-    monday.setHours(0, 0, 0, 0);
+    const startDate = new Date(now);
+    const startDayOffset =
+        currentDay >= startDay
+            ? currentDay - startDay
+            : 7 - (startDay - currentDay);
+    startDate.setDate(now.getDate() - startDayOffset + daysToAdjust);
+    startDate.setHours(startHour, 0, 0, 0);
 
-    const sunday = new Date(monday);
-    sunday.setDate(monday.getDate() + 6);
-    sunday.setHours(23, 59, 59, 999);
+    const endDate = new Date(startDate);
+    endDate.setDate(startDate.getDate() + 6);
+    endDate.setHours(startHour - 1, 59, 59, 999);
 
     return {
-        startDate: monday.toISOString(),
-        endDate: sunday.toISOString(),
+        startDate: startDate.toISOString(),
+        endDate: endDate.toISOString(),
     };
 };
 
