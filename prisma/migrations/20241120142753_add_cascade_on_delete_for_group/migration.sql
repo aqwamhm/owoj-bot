24ac0060685e781e98e5c785afa4f756fee4751b
Aqwam Hizbal Muhshiy <aqwam.hizbal@outlook.com>
2024-11-21 10:11:43 +0700

    Fix migration table lowercase

--- prisma/migrations/20241120142753_add_cascade_on_delete_for_group/migration.sql
+++ prisma/migrations/20241120142753_add_cascade_on_delete_for_group/migration.sql
@@ -1,5 +1,5 @@
 -- DropForeignKey
-ALTER TABLE `member` DROP FOREIGN KEY `Member_groupId_fkey`;
+ALTER TABLE `Member` DROP FOREIGN KEY `Member_groupId_fkey`;
 
 -- AddForeignKey
 ALTER TABLE `Member` ADD CONSTRAINT `Member_groupId_fkey` FOREIGN KEY (`groupId`) REFERENCES `Group`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
