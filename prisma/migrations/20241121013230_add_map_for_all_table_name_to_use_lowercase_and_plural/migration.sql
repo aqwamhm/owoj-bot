9e879bbef47e34670d5d12037336f7eec3a29a89
Aqwam Hizbal Muhshiy <aqwam.hizbal@outlook.com>
2024-11-21 10:28:08 +0700

    Update miration to use RENAME instead of DROP

--- prisma/migrations/20241121013230_add_map_for_all_table_name_to_use_lowercase_and_plural/migration.sql
+++ prisma/migrations/20241121013230_add_map_for_all_table_name_to_use_lowercase_and_plural/migration.sql
@@ -1,98 +1,16 @@
-/*
-  Warnings:
+-- Rename the existing tables
+RENAME TABLE `Admin` TO `admins`;
+RENAME TABLE `Group` TO `groups`;
+RENAME TABLE `Member` TO `members`;
+RENAME TABLE `Period` TO `periods`;
+RENAME TABLE `Report` TO `reports`;
 
-  - You are about to drop the `admin` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `group` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `member` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `period` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `report` table. If the table is not empty, all the data it contains will be lost.
+-- Update foreign key constraints with new table names
+ALTER TABLE `members`
+    ADD CONSTRAINT `members_groupId_fkey` FOREIGN KEY (`groupId`) REFERENCES `groups`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
-*/
--- DropForeignKey
-ALTER TABLE `member` DROP FOREIGN KEY `Member_groupId_fkey`;
+ALTER TABLE `reports`
+    ADD CONSTRAINT `reports_memberName_memberGroupId_fkey` FOREIGN KEY (`memberName`, `memberGroupId`) REFERENCES `members`(`name`, `groupId`) ON DELETE CASCADE ON UPDATE CASCADE;
 
--- DropForeignKey
-ALTER TABLE `report` DROP FOREIGN KEY `Report_memberName_memberGroupId_fkey`;
-
--- DropForeignKey
-ALTER TABLE `report` DROP FOREIGN KEY `Report_periodStartDate_periodEndDate_fkey`;
-
--- DropTable
-DROP TABLE `admin`;
-
--- DropTable
-DROP TABLE `group`;
-
--- DropTable
-DROP TABLE `member`;
-
--- DropTable
-DROP TABLE `period`;
-
--- DropTable
-DROP TABLE `report`;
-
--- CreateTable
-CREATE TABLE `members` (
-    `name` VARCHAR(191) NOT NULL,
-    `currentJuz` INTEGER NOT NULL,
-    `groupId` VARCHAR(191) NOT NULL,
-    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    `updatedAt` DATETIME(3) NOT NULL,
-
-    PRIMARY KEY (`name`, `groupId`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- CreateTable
-CREATE TABLE `groups` (
-    `id` VARCHAR(191) NOT NULL,
-    `number` INTEGER NOT NULL,
-    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-
-    UNIQUE INDEX `groups_number_key`(`number`),
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- CreateTable
-CREATE TABLE `reports` (
-    `id` VARCHAR(191) NOT NULL,
-    `memberName` VARCHAR(191) NOT NULL,
-    `memberGroupId` VARCHAR(191) NOT NULL,
-    `juz` INTEGER NOT NULL,
-    `pages` INTEGER NOT NULL,
-    `totalPages` INTEGER NOT NULL DEFAULT 0,
-    `type` ENUM('TILAWAH', 'TERJEMAH', 'MUROTTAL') NOT NULL DEFAULT 'TILAWAH',
-    `periodStartDate` DATETIME(3) NOT NULL,
-    `periodEndDate` DATETIME(3) NOT NULL,
-    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    `updatedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-
-    UNIQUE INDEX `reports_memberName_memberGroupId_pages_totalPages_type_perio_key`(`memberName`, `memberGroupId`, `pages`, `totalPages`, `type`, `periodStartDate`, `periodEndDate`),
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- CreateTable
-CREATE TABLE `periods` (
-    `startDate` DATETIME(3) NOT NULL,
-    `endDate` DATETIME(3) NOT NULL,
-    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-
-    PRIMARY KEY (`startDate`, `endDate`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- CreateTable
-CREATE TABLE `admins` (
-    `phoneNumber` VARCHAR(191) NOT NULL,
-    `name` VARCHAR(191) NOT NULL,
-
-    PRIMARY KEY (`phoneNumber`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- AddForeignKey
-ALTER TABLE `members` ADD CONSTRAINT `members_groupId_fkey` FOREIGN KEY (`groupId`) REFERENCES `groups`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE `reports` ADD CONSTRAINT `reports_memberName_memberGroupId_fkey` FOREIGN KEY (`memberName`, `memberGroupId`) REFERENCES `members`(`name`, `groupId`) ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE `reports` ADD CONSTRAINT `reports_periodStartDate_periodEndDate_fkey` FOREIGN KEY (`periodStartDate`, `periodEndDate`) REFERENCES `periods`(`startDate`, `endDate`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `reports`
+    ADD CONSTRAINT `reports_periodStartDate_periodEndDate_fkey` FOREIGN KEY (`periodStartDate`, `periodEndDate`) REFERENCES `periods`(`startDate`, `endDate`) ON DELETE RESTRICT ON UPDATE CASCADE;
