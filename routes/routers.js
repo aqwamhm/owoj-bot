d8de9232dac6a73805edcfc64be9841fffb72dd7
Aqwam Hizbal Muhshiy <aqwam.hizbal@outlook.com>
2025-03-01 14:55:16 +0700

    Update catch error log

--- routes/routers.js
+++ routes/routers.js
@@ -60,8 +60,8 @@ const commandRouter = async (message, client) => {
                     },
                     { quoted: message }
                 );
+                console.error(e);
             }
-            console.error(e);
         }
     }
 };
