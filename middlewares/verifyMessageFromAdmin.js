cc4274589de0027c91aa101d94749267bc45e68c
Aqwam Hizbal Muhshiy <aqwam.hizbal@outlook.com>
2025-08-02 09:01:51 +0700

    Update verifyMessageFromAdmin middleware

--- middlewares/verifyMessageFromAdmin.js
+++ middlewares/verifyMessageFromAdmin.js
@@ -2,11 +2,17 @@ const AuthorizationError = require("../exceptions/AuthorizationError");
 const adminServices = require("../services/admin");
 
 const verifyMessageFromAdmin = async (message) => {
-    const phoneNumber = message.key.remoteJid?.split("@")[0];
+    const isGroupMessage = message.key.remoteJid?.endsWith("@g.us");
+
+    const senderJid = isGroupMessage
+        ? message.key.participant
+        : message.key.remoteJid;
+
+    const phoneNumber = senderJid?.split("@")[0];
 
     const admin = await adminServices.find({ phoneNumber });
     if (!admin) {
-        console.log(phoneNumber, "bukan admin");
+        console.error(`Unauthorized access attempt by ${phoneNumber}`);
 
         throw new AuthorizationError(
             `Tidak dapat menjalankan command ini. Nomor anda tidak terdaftar sebagai admin.`
