2d56467cccaefb38bcaa24cd4473102fb962f958
Aqwam Hizbal Muhshiy <aqwam.hizbal@outlook.com>
2025-08-04 09:06:03 +0700

    Update verifyMessageFromAdmin

--- middlewares/verifyMessageFromAdmin.js
+++ middlewares/verifyMessageFromAdmin.js
@@ -2,13 +2,13 @@ const AuthorizationError = require("../exceptions/AuthorizationError");
 const adminServices = require("../services/admin");
 
 const verifyMessageFromAdmin = async (message) => {
-    const isGroupMessage = message.key.remoteJid?.endsWith("@g.us");
+    const isGroup = message.key.remoteJid?.endsWith("@g.us");
 
-    const senderJid = isGroupMessage
-        ? message.key.participant
+    const senderJid = isGroup
+        ? message.key.participant || message.key.remoteJid
         : message.key.remoteJid;
 
-    const phoneNumber = senderJid?.split("@")[0];
+    const phoneNumber = senderJid.split("@")[0];
 
     const admin = await adminServices.find({ phoneNumber });
     if (!admin) {
